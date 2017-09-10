4ba528a04ee20b05ec6fd07751621c0320439dfc
Tobias Reusing <tobias@reusing.eu>
2017-09-10 14:53:51 +0200

    Some mild refactoring

--- main.go
+++ main.go
@@ -67,61 +67,74 @@ func rewriteCSV(inputFileName string, outputFileName string, fromDate time.Time,
 	defer writer.Flush()
 
 	hasReference := false
+	headerFound := false
 	for scanner.Scan() {
 		inputTokens := strings.Split(scanner.Text(), ";")
+		if !headerFound {
+			headerFound, hasReference = checkHeader(inputTokens)
+		}
+		if !headerFound {
+			continue
+		}
 
 		if len(inputTokens) < 5 {
 			continue
 		}
 		outputTokens := make([]string, 6)
 
-		if !hasReference && strings.Contains(inputTokens[3], "Referenz") {
-			hasReference = true
-		}
 		if hasReference {
 			inputTokens = append(inputTokens[:3], inputTokens[4:]...)
 		}
-		if strings.Contains(inputTokens[0], "Buchungstag") {
-			outputTokens = strings.Split("Date;Payee;Category;Memo;Outflow;Inflow", ";")
-		} else {
-			outputTokens[0] = removeQuotes(inputTokens[0])
-			date, _ := time.Parse(dateFormat, outputTokens[0])
-			if date.Before(fromDate) {
-				continue
-			}
-			rawValue := strings.Replace(removeQuotes(inputTokens[4]), ".", "", -1)
-			value, _ := strconv.ParseFloat(strings.Replace(rawValue, ",", ".", 1), 64)
-			valueStr := strconv.FormatFloat(math.Abs(value), 'f', 2, 64)
-			if value < 0 {
-				outputTokens[4] = valueStr
-			} else {
-				outputTokens[5] = valueStr
-			}
 
-			if strings.Contains(inputTokens[2], "Lastschrift") {
-				s := strings.Split(inputTokens[3], " Buchungstext: ")
-				outputTokens[3] = removeQuotes(s[1])
-				outputTokens[1] = removeQuotes(strings.Replace(s[0], "Auftraggeber: ", "", 1))
-			} else if strings.Contains(inputTokens[2], "Wertpapiere") {
-				outputTokens[3] = removeQuotes(strings.Replace(inputTokens[3], "Buchungstext: ", "", 1))
-				outputTokens[1] = "Transfer: .comdirect Depot"
-			} else if strings.Contains(inputTokens[2], "Überweisung") {
-				s := strings.Split(inputTokens[3], " Buchungstext: ")
-				outputTokens[3] = removeQuotes(s[1])
-				outputTokens[1] = removeQuotes(strings.Replace(s[0], "Empfänger: ", "", 1))
-			} else if strings.Contains(inputTokens[2], "Visa-Umsatz") {
-				outputTokens[1] = removeQuotes(inputTokens[3])
-			} else if strings.Contains(inputTokens[2], "Visa-Kartenabrechnung") {
-				outputTokens[1] = "Transfer: .comdirect"
-			} else {
-				outputTokens[3] = removeQuotes(inputTokens[3])
-			}
+		outputTokens[0] = removeQuotes(inputTokens[0])
+		date, _ := time.Parse(dateFormat, outputTokens[0])
+		if date.Before(fromDate) {
+			continue
 		}
+		rawValue := strings.Replace(removeQuotes(inputTokens[4]), ".", "", -1)
+		value, _ := strconv.ParseFloat(strings.Replace(rawValue, ",", ".", 1), 64)
+		valueStr := strconv.FormatFloat(math.Abs(value), 'f', 2, 64)
+		if value < 0 {
+			outputTokens[4] = valueStr
+		} else {
+			outputTokens[5] = valueStr
+		}
+
+		if strings.Contains(inputTokens[2], "Lastschrift") {
+			s := strings.Split(inputTokens[3], " Buchungstext: ")
+			outputTokens[3] = removeQuotes(s[1])
+			outputTokens[1] = removeQuotes(strings.Replace(s[0], "Auftraggeber: ", "", 1))
+		} else if strings.Contains(inputTokens[2], "Wertpapiere") {
+			outputTokens[3] = removeQuotes(strings.Replace(inputTokens[3], "Buchungstext: ", "", 1))
+			outputTokens[1] = "Transfer: .comdirect Depot"
+		} else if strings.Contains(inputTokens[2], "Überweisung") {
+			s := strings.Split(inputTokens[3], " Buchungstext: ")
+			outputTokens[3] = removeQuotes(s[1])
+			outputTokens[1] = removeQuotes(strings.Replace(s[0], "Empfänger: ", "", 1))
+		} else if strings.Contains(inputTokens[2], "Visa-Umsatz") {
+			outputTokens[1] = removeQuotes(inputTokens[3])
+		} else if strings.Contains(inputTokens[2], "Visa-Kartenabrechnung") {
+			outputTokens[1] = "Transfer: .comdirect"
+		} else {
+			outputTokens[3] = removeQuotes(inputTokens[3])
+		}
+
 		outputTokens[1], outputTokens[3] = substitution.substitute(outputTokens[1], outputTokens[3])
 		filterRef(outputTokens)
 
 		fmt.Fprintln(writer, strings.Join(outputTokens, ","))
 	}
+}
+
+func checkHeader(tokens []string) (bool, bool) {
+	isHeader := strings.Contains(tokens[0], "Buchungstag")
+
+	hasReference := false
+
+	if isHeader {
+		hasReference = strings.Contains(tokens[3], "Referenz")
+	}
+	return isHeader, hasReference
 }
 
 func removeQuotes(s string) string {
