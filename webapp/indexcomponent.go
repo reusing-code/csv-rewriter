7c2c559ec590341637f97d6f72a90beef4ab631b
Tobias Reusing <tobias@reusing.eu>
2018-02-19 19:31:29 +0100

    Brought webapp in a working state

--- webapp/indexcomponent.go
+++ webapp/indexcomponent.go
@@ -4,9 +4,27 @@ type IndexComponent struct {
 }
 
 func (c *IndexComponent) Render() string {
-	html := `<div class="container-fluid">
-    		   <div id="app"></div>
-             </div>`
+	html :=
+		`
+<div class="container">
+<h1 class="display-1">csvrewrite</h1>
+<p class="lead">Rewrite .comdirect csv to YNAB4 compatible csv</p>
+<form>
+  <div class="form-group">
+    <label for="csvinput">Input .csv file</label>
+    <input type="file" class="form-control-file" id="csvinput">
+  </div>
+</form>
+<div id="rewrite-output-container" hidden>
+<h3>Warnings</h3>
+<div class="alert alert-warning" role="alert">
+<pre>
+<samp id="rewrite-output-content">
+</samp>
+</pre>
+</div>
+</div>
+</div>`
 
 	return html
 
